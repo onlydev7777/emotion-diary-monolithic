05097c0161ef20922faafbbbd12b9eecf7e689d8
onlydev7777 <onlydev7777@gmail.com>
2024-07-23 18:12:50 +0900

    1) Diary 데이터 초기화

--- src/main/java/com/example/emotiondiarymember/InitData.java
+++ src/main/java/com/example/emotiondiarymember/InitData.java
@@ -1,11 +1,16 @@
 package com.example.emotiondiarymember;
 
+import com.example.emotiondiarymember.constant.EmotionStatus;
 import com.example.emotiondiarymember.constant.SocialType;
+import com.example.emotiondiarymember.entity.Diary;
 import com.example.emotiondiarymember.entity.Member;
 import com.example.emotiondiarymember.entity.embeddable.Email;
 import com.example.emotiondiarymember.entity.embeddable.Password;
+import com.example.emotiondiarymember.repository.DiaryRepository;
 import com.example.emotiondiarymember.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,16 +20,34 @@ import org.springframework.stereotype.Component;
 public class InitData {
 
   private final MemberRepository memberRepository;
+  private final DiaryRepository diaryRepository;
   private final PasswordEncoder passwordEncoder;
 
   @PostConstruct
   public void init() {
-    memberRepository.save(Member.of(
+    Member savedMember = memberRepository.save(Member.of(
         "test",
         new Password("qwer1234!", passwordEncoder),
         "테스트",
         new Email("test@test.com"),
         SocialType.NONE)
+    );
+
+    diaryRepository.saveAll(
+        List.of(Diary.of(
+                "테스트 제목1",
+                "테스트 내용1",
+                EmotionStatus.GREAT,
+                LocalDate.now(),
+                savedMember
+            ),
+            Diary.of(
+                "테스트 제목2",
+                "테스트 내용2",
+                EmotionStatus.BAD,
+                LocalDate.now(),
+                savedMember
+            ))
     );
   }
 }
--- src/main/java/com/example/emotiondiarymember/constant/EmotionStatus.java
+++ src/main/java/com/example/emotiondiarymember/constant/EmotionStatus.java
@@ -1,5 +1,14 @@
 package com.example.emotiondiarymember.constant;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum EmotionStatus {
-  GREAT, GOOD, NORMAL, BAD, VERY_BAD
+  GREAT("1"), GOOD("2"), NORMAL("3"), BAD("4"), VERY_BAD("5");
+
+  @JsonValue
+  private final String id;
 }
